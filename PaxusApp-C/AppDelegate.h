b7f4eec51fa0f5009637196c12abfdd21a12c605
jithinpala <jithinpala@gmail.com>
2018-02-14 09:10:15 +1100

    Initial Commit

---
+++ PaxusApp-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaxusApp-C
+//
+//  Created by Jithin on 14/2/18.
+//  Copyright © 2018 Jithinpala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaxusApp-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaxusApp-C
+//
+//  Created by Jithin on 14/2/18.
+//  Copyright © 2018 Jithinpala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
